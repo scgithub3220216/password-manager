958ebfab470f01a38b58b2c944b838b04e65a64a
sc <17786925624@163.com>
2024-07-05 10:02:06 +0800

    style :support

--- src/components/setview/Support.vue
+++ src/components/setview/Support.vue
@@ -7,7 +7,18 @@ const {openBrowser} = useBrowser();
 </script>
 
 <template>
-  <p><b>郑重声明: 所有收入全部用于公益</b></p>
+  <p><b>郑重声明: 所有收入全部用于
+    <el-tooltip
+        class="box-item"
+        effect="dark"
+        content="孤寡老人、残疾人、贫困家庭等群体的帮助。"
+        placement="top-start"
+    >
+      <span style="color: #4b63ba">
+      公益
+      </span>
+    </el-tooltip>
+  </b></p>
   <p>详细收支清单:
     <span @click="openBrowser(supportLink)">网页链接</span>
   </p>
