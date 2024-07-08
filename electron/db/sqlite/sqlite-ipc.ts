4e344bf23338158a1258c0d34cfd65324ceb9db5
sc <17786925624@163.com>
2024-07-08 08:21:33 +0800

    style: 警告抑制

--- electron/db/sqlite/sqlite-ipc.ts
+++ electron/db/sqlite/sqlite-ipc.ts
@@ -31,13 +31,13 @@ import {getShortcutKey, updateShortcutKey} from "./mapper/shortcutKey.ts";
 export const SQLiteIPC = () => {
     // config
     // ipc sqlite update data
-    ipcMain.handle(IPC_SQLITE_UPDATE_CONFIG_DATA, async (event, ...args) => {
+    ipcMain.handle(IPC_SQLITE_UPDATE_CONFIG_DATA, async (_event, ...args) => {
         console.log(`IPC_SQLITE_UPDATE_CONFIG_DATA  args : ${args}`);
         return await updateConfig(...args);
     });
 
     // ipc sqlite select data
-    ipcMain.handle(IPC_SQLITE_SELECT_CONFIG_DATA, async (event, args) => {
+    ipcMain.handle(IPC_SQLITE_SELECT_CONFIG_DATA, async (_event, args) => {
         console.log(`IPC_SQLITE_SELECT_CONFIG_DATA  args : ${args}`);
         return await getConfig(args);
     });
@@ -45,71 +45,71 @@ export const SQLiteIPC = () => {
 
     // group
     // ipc sqlite insert data
-    ipcMain.handle(IPC_SQLITE_INSERT_GROUP_DATA, async (event, ...args) => {
+    ipcMain.handle(IPC_SQLITE_INSERT_GROUP_DATA, async (_event, ...args) => {
         console.log(`IPC_SQLITE_INSERT_GROUP_DATA  args : ${args}`);
         return await insertGroup(...args);
     });
 
     // ipc sqlite delete data
-    ipcMain.handle(IPC_SQLITE_DELETE_GROUP_DATA, async (event, args) => {
+    ipcMain.handle(IPC_SQLITE_DELETE_GROUP_DATA, async (_event, args) => {
         console.log(`IPC_SQLITE_DELETE_GROUP_DATA  args : ${args}`);
         return await delGroup(args);
     });
 
 
     // ipc sqlite update data
-    ipcMain.handle(IPC_SQLITE_UPDATE_GROUP_DATA, async (event, ...args) => {
+    ipcMain.handle(IPC_SQLITE_UPDATE_GROUP_DATA, async (_event, ...args) => {
         console.log(`IPC_SQLITE_UPDATE_GROUP_DATA  args : ${args}`);
         return await updateGroup(...args);
     });
 
     // ipc sqlite select data
-    ipcMain.handle(IPC_SQLITE_SELECT_GROUP_DATA, async (event, args) => {
+    ipcMain.handle(IPC_SQLITE_SELECT_GROUP_DATA, async (_event, args) => {
         console.log(`IPC_SQLITE_SELECT_GROUP_DATA  args : ${args}`);
         return await listGroup();
     });
 
     // pwdInfo
     // ipc sqlite insert data
-    ipcMain.handle(IPC_SQLITE_INSERT_PWD_INFO_DATA, async (event, ...args) => {
+    ipcMain.handle(IPC_SQLITE_INSERT_PWD_INFO_DATA, async (_event, ...args) => {
         console.log(`IPC_SQLITE_INSERT_PWD_INFO_DATA  args : ${args}`);
         return await insertPwdInfo(...args);
     });
 
     // ipc sqlite delete data
-    ipcMain.handle(IPC_SQLITE_DELETE_PWD_INFO_DATA, async (event, args) => {
+    ipcMain.handle(IPC_SQLITE_DELETE_PWD_INFO_DATA, async (_event, args) => {
         console.log(`IPC_SQLITE_DELETE_PWD_INFO_DATA  args : ${args}`);
         return await delPwdInfo(args);
     });
 
 
     // ipc sqlite update data
-    ipcMain.handle(IPC_SQLITE_UPDATE_PWD_INFO_DATA, async (event, ...args) => {
+    ipcMain.handle(IPC_SQLITE_UPDATE_PWD_INFO_DATA, async (_event, ...args) => {
         console.log(`IPC_SQLITE_UPDATE_PWD_INFO_DATA  args : ${args}`);
         return await updatePwdInfo(...args);
     });
 
     // ipc sqlite select list data
-    ipcMain.handle(IPC_SQLITE_SELECT_LIST_PWD_INFO_DATA, async (event, args) => {
+    ipcMain.handle(IPC_SQLITE_SELECT_LIST_PWD_INFO_DATA, async (_event, args) => {
         console.log(`IPC_SQLITE_SELECT_PWD_INFO_DATA  args : ${args}`);
         return await listPwdInfo(args);
     });
 
     // ipc sqlite select get data
-    ipcMain.handle(IPC_SQLITE_SELECT_GET_PWD_INFO_DATA, async (event, args) => {
+    ipcMain.handle(IPC_SQLITE_SELECT_GET_PWD_INFO_DATA, async (_event, args) => {
         console.log(`IPC_SQLITE_SELECT_GET_PWD_INFO_DATA  args : ${args}`);
         return await getPwdInfo(args);
     });
 
     // shortcutKey
     // ipc sqlite update data
-    ipcMain.handle(IPC_SQLITE_UPDATE_SHORTCUT_KEY_DATA, async (event, ...args) => {
+    ipcMain.handle(IPC_SQLITE_UPDATE_SHORTCUT_KEY_DATA, async (_event, ...args) => {
         console.log(`IPC_SQLITE_UPDATE_SHORTCUT_KEY_DATA  args : ${args}`);
         return await updateShortcutKey(...args);
     });
 
     // ipc sqlite select data
-    ipcMain.handle(IPC_SQLITE_SELECT_SHORTCUT_KEY_DATA, async (event, args) => {
+    ipcMain.handle(IPC_SQLITE_SELECT_SHORTCUT_KEY_DATA, async (_event, args) => {
         console.log(`IPC_SQLITE_SELECT_SHORTCUT_KEY_DATA  args : ${args}`);
         return await getShortcutKey(args);
     });
