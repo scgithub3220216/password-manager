3ba597ff5209fc27aed69c07d5d8e174095f4bc7
sc <17786925624@163.com>
2024-07-10 13:48:41 +0800

    feat:  top menu2

--- src/components/indexview/TopMenu.vue
+++ src/components/indexview/TopMenu.vue
@@ -1,21 +1,43 @@
 <script setup lang="ts">
 
-import {Menu} from "@element-plus/icons-vue";
+import {Menu} from '@element-plus/icons-vue'
 
-function clickMenu() {
-  console.log('clickMenu')
+function clickImport() {
+  console.log('clickImport')
+}
+
+function clickExport() {
+  console.log('clickExport')
 }
 </script>
 
 <template>
-  <!--菜单-->
-  <el-button class="btn" @click="clickMenu">
-    <el-icon>
-      <Menu/>
-    </el-icon>
-  </el-button>
+
+  <el-dropdown>
+    <el-button class="btn">
+      <el-icon>
+        <Menu/>
+      </el-icon>
+    </el-button>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item @click="clickImport">导入</el-dropdown-item>
+        <el-dropdown-item @click="clickExport">导出</el-dropdown-item>
+        <el-dropdown-item>帮助</el-dropdown-item>
+        <el-dropdown-item>关于</el-dropdown-item>
+        <el-dropdown-item>开发</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+
 </template>
 
 <style scoped>
+.btn {
+  height: 100%;
+  margin: 0;
+  border: none;
+  background: none;
+}
 
 </style>
