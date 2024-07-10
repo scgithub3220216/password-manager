d197c7ff5baaccf9ae3403fa7484daaad6c04aa9
sc <17786925624@163.com>
2024-07-10 13:35:23 +0800

    feat: 导入修复 数字 为22.0的问题

--- src/hooks/useExcel.ts
+++ src/hooks/useExcel.ts
@@ -10,11 +10,20 @@ export default function () {
     const {insertPwdInfoByImport, listPwdInfo} = useDBPwdInfo()
     const {insertGroup, getIdByTitle} = useDBGroup();
     const userDataInfoStore = useUserDataInfoStore();
+
+    const groupStr = '分组';
+    const titleStr = '标题';
+    const usernameStr = '用户名';
+    const pwdStr = '密码';
+    const linkStr = '链接';
+    const remarkStr = '说明';
+
     const exportExcel = async () => {
         console.log('exportExcel')
         // 创建工作表的数据
+
         const dataList = [
-            ['分组', '标题', '用户名', '密码', '链接', '说明']
+            [groupStr, titleStr, usernameStr, pwdStr, linkStr, remarkStr]
         ];
         // 获取 所有数据
         let pwdInfoList: PwdInfo[] = await listPwdInfo(0)
@@ -41,16 +50,17 @@ export default function () {
             const workbook = XLSX.read(data, {type: 'binary'});
             const sheetName = workbook.SheetNames[0];
             const sheet = workbook.Sheets[sheetName];
-            const jsonArr = XLSX.utils.sheet_to_json(sheet);
+            const jsonArr: { [key: string]: any }[] = XLSX.utils.sheet_to_json(sheet);
             console.log(jsonArr); // 打印导入的数据
             for (const item of jsonArr) {
+                // @ts-ignore
                 let pwdInfo: PwdInfo = {
-                    group_title: item['分组'],
-                    title: item['标题'],
-                    username: item['用户名'],
-                    password: item['密码'],
-                    link: item['链接'],
-                    remark: item['说明'],
+                    group_title: item[groupStr] ? String(item[groupStr]) : '默认分组',
+                    title: item[titleStr] ? String(item[titleStr]) : '',
+                    username: item[usernameStr] ? String(item[usernameStr]) : '',
+                    password: item[pwdStr] ? String(item[pwdStr]) : '',
+                    link: item[linkStr] ? String(item[linkStr]) : '',
+                    remark: item[remarkStr] ? String(item[remarkStr]) : '',
                 }
                 // 根据 group_title 查询分组
                 let groupId = await getIdByTitle(pwdInfo.group_title);
