153a9f25d747f45a685148de7e5f734d60d7db99
sc <17786925624@163.com>
2024-07-10 13:51:27 +0800

    feat:  topMenu 导出

--- src/components/indexview/TopMenu.vue
+++ src/components/indexview/TopMenu.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 
 import {Menu} from '@element-plus/icons-vue'
+import useExcel from "../../hooks/useExcel.ts";
+import {ref} from "vue";
+
+const {exportExcel} = useExcel();
+const importRef = ref();
 
 function clickImport() {
   console.log('clickImport')
 }
 
-function clickExport() {
-  console.log('clickExport')
-}
 </script>
 
 <template>
@@ -22,13 +24,14 @@ function clickExport() {
     <template #dropdown>
       <el-dropdown-menu>
         <el-dropdown-item @click="clickImport">导入</el-dropdown-item>
-        <el-dropdown-item @click="clickExport">导出</el-dropdown-item>
+        <el-dropdown-item @click="exportExcel">导出</el-dropdown-item>
         <el-dropdown-item>帮助</el-dropdown-item>
         <el-dropdown-item>关于</el-dropdown-item>
         <el-dropdown-item>开发</el-dropdown-item>
       </el-dropdown-menu>
     </template>
   </el-dropdown>
+<!--  <Import ref="importRef" />-->
 
 </template>
 
