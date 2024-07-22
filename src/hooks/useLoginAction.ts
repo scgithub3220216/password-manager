ebe6c2b5b75a2cfb55ee9e7f726aa24271de7810
sc <17786925624@163.com>
2024-07-22 11:01:52 +0800

    fix: 搜索后退出登录 情况搜索结果

--- src/hooks/useLoginAction.ts
+++ src/hooks/useLoginAction.ts
@@ -1,8 +1,9 @@
 import {useUserDataInfoStore} from "../store/userDataInfo.ts";
+import {useSearchResultStore} from "../store/searchResult.ts";
 
 export default function () {
     const userInfoStore = useUserDataInfoStore();
-
+    const searchResultStore = useSearchResultStore();
     function login() {
         window.location.hash = '/index'
         userInfoStore.login()
@@ -11,6 +12,7 @@ export default function () {
     function logout() {
         window.location.hash = '/login'
         userInfoStore.logout()
+        searchResultStore.closeSearchView()
     }
 
     return {login, logout};
