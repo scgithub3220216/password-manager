871cb1cf08c12db7037a6b7b9ee3bc664efa01c8
sc <17786925624@163.com>
2025-09-08 16:37:24 +0800

    feat:code style

--- src/store/cssSwitch.ts
+++ src/store/cssSwitch.ts
@@ -1,12 +1,9 @@
 import {defineStore} from 'pinia'
 import {ref} from "vue";
-import {useUserDataInfoStore} from "./userDataInfo.ts";
 
 export const useCssSwitchStore = defineStore('cssSwitch', () => {
     const curGroupIndex = ref(-1)
     const curPwdListIndex = ref(-1)
-
-    const userDataInfoStore = useUserDataInfoStore()
 
     function setGroupIndex(index: number) {
         curGroupIndex.value = index;
@@ -16,14 +13,5 @@ export const useCssSwitchStore = defineStore('cssSwitch', () => {
         curPwdListIndex.value = index;
     }
 
-    function addGroupSwitch() {
-        curGroupIndex.value = userDataInfoStore.getGroupLength() - 1;
-    }
-
-    function addPwdListSwitch() {
-        curPwdListIndex.value = userDataInfoStore.getPwdListLength() - 1;
-    }
-
-
-    return {curGroupIndex, curPwdListIndex, setGroupIndex, setPwdListIndex, addGroupSwitch, addPwdListSwitch}
+    return {curGroupIndex, curPwdListIndex, setGroupIndex, setPwdListIndex}
 })
