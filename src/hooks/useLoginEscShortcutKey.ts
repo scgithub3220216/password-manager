8e5d655985e2f89bc3a6ca82cd8d6f462630be27
sc <17786925624@163.com>
2026-01-10 19:04:33 +0800

    fix:登录页面按esc 则最小化到托盘

--- src/hooks/useLoginEscShortcutKey.ts
+++ src/hooks/useLoginEscShortcutKey.ts
@@ -28,18 +28,15 @@ export default function () {
 
     function handleKeyUp(event: any) {
         // console.log('handleKeyUp:', event.key)
-
         pressedKeys.delete(convertCtrlKey(event)?.trim());
     }
 
     function checkCombinations() {
         console.log('checkCombinations pressedKeys:', pressedKeys)
         // console.log('checkCombinations shortCutKeyCombs:', shortCutKeyCombs.value)
-        // 如果按下的是ecs ,则打印日志
         if (pressedKeys.has('escape')) {
-            console.log(`pressed esc `)
-            // 最小化
-            console.log(`最小化窗口`)
+            console.log(`pressed esc 最小化窗口 `)
+            pressedKeys.clear()
             window.ipcRenderer.invoke(IPC_CLOSE_WIN);
         }
 
